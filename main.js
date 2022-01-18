8c733fbe77e84a5179e2801269e2675ba4465e41
manyvh <josemanuelvarela36@gmail.com>
2022-01-17 22:22:17 -0600

    Función Jugador

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
     const botn1 = document.getElementById('casilla1');
+    const botn2 = document.getElementById('casilla2');
+    const botn3 = document.getElementById('casilla3');
+    const botn4 = document.getElementById('casilla4');
+    const botn5 = document.getElementById('casilla5');
+    const botn6 = document.getElementById('casilla6');
+    const botn7 = document.getElementById('casilla7');
+    const botn8 = document.getElementById('casilla8');
+    const botn9 = document.getElementById('casilla9');
+    
+    const turno = document.getElementById('turno');
     botn1.onclick = cambio;
+    botn2.onclick = cambio;
+    botn3.onclick = cambio;
+    botn4.onclick = cambio;
+    botn5.onclick = cambio;
+    botn6.onclick = cambio;
+    botn7.onclick = cambio;
+    botn8.onclick = cambio;
+    botn9.onclick = cambio;
     
     function cambio() {
+        let juagdor = turno.textContent;
         
-        const imga = this.childNodes;
-        document.getElementById(imga[0].id).setAttribute('style','visibility: visible;')
-        console.log(imga[0].id)
-        this.disabled = true;
+        if (juagdor.toString()==='1') {
+            const imga = this.childNodes;
+            document.getElementById(imga[0].id).setAttribute('style','visibility: visible;');
+            document.getElementById(imga[0].id).setAttribute('src','Img/borrar.png');
+            console.log(imga[0].id);
+            this.disabled = true;
+            turno.textContent = '2';
+        } else {
+            const imga = this.childNodes;
+            document.getElementById(imga[0].id).setAttribute('style','visibility: visible;');
+            document.getElementById(imga[0].id).setAttribute('src','Img/o.png');
+            console.log(imga[0].id);
+            this.disabled = true;
+            turno.textContent = '1';
+        }
+        
     }
-})
+});
